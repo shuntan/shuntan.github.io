d81b30bee547d2f42d4f21edca1af01b7c0c64eb
shuntan <shawntan@tencent.com>
2019-05-17 21:26:08 +0800

    Site updated: 2019-05-17 21:26:07

---
+++ js/wrapper.js
@@ -0,0 +1,35 @@
+	// jQuery Selections
+	var $html = $('html'),
+			$container = $('#container-parallax'),
+			$prompt = $('#prompt'),
+			$scene = $('#scene');
+
+	// Hide browser menu.
+	(function() {
+		setTimeout(function(){window.scrollTo(0,0);},0);
+	})();
+
+	// Setup FastClick.
+	FastClick.attach(document.body);
+
+	// Add touch functionality.
+	if (Hammer.HAS_TOUCHEVENTS) {
+		$container.hammer({drag_lock_to_axis: true});
+		_.tap($html, 'a,button,[data-tap]');
+	}
+
+	// Add touch or mouse class to html element.
+	$html.addClass(Hammer.HAS_TOUCHEVENTS ? 'touch' : 'mouse');
+
+	// Resize handler.
+	(resize = function() {
+		$scene[0].style.width = window.innerWidth + 'px';
+		$scene[0].style.height = window.innerHeight + 'px';
+	})();
+
+	// Attach window listeners.
+	window.onresize = _.debounce(resize, 200);
+	window.onscroll = _.debounce(resize, 200);
+
+	// Pretty simple huh?
+	$scene.parallax();
